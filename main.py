cf110fa47f92f0c5797a742c8b77f7661bccf1d2
JP Zhang <zjplab@gmail.com>
2019-04-08 10:09:27 -0400

    data_loader

--- main.py
+++ main.py
@@ -1,4 +1,47 @@
+import argparse
+# Importing the libraries
+import os
+import numpy as np
+import pandas as pd
+from random import sample
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import BatchSampler, SequentialSampler
+import pickle
+from model import *
+import data_loader
 
+parser = argparse.ArgumentParser()
+# data
+parser.add_argument('--mode', type=str, default="train", help='train / test')
+parser.add_argument('--data_type', type=str, default="ml_100k")
+parser.add_argument('--model-path', type=str, default="./models")
+parser.add_argument('--data-path', type=str, default="./data")
+parser.add_argument('--data-shuffle', type=bool, default=True)
+parser.add_argument('--batch-size', type=int, default=512)
+parser.add_argument('--num_epochs', type=int, default=100)
+parser.add_argument('--val-step', type=int, default=5)
+parser.add_argument('--test-epoch', type=int, default=50)
+parser.add_argument('--start-epoch', type=int, default=0)
+parser.add_argument('--neg-cnt', type=int, default=100)
+parser.add_argument('--lr', type=float, default=0.01)
+parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for Adam optimizer')
+parser.add_argument('--beta2', type=float, default=0.999, help='beta2 for Adam optimizer')
+parser.add_argument('--dropout', type=float, default=0.7)
+parser.add_argument('--n_critic', type=int, default=5, help='number of D updates per each G update')
+
+parser.add_argument('--emb-dim', type=int, default=32)
+parser.add_argument('--hidden', default=[64,32,16, 8])
+parser.add_argument('--nb', type=int, default=2)
+
+parser.add_argument('--train_path', '-train',type=str, default='/rating_0.pkl')#train.pkl')
+parser.add_argument('--val_path','-val', type=str, default='/rating_1.pkl')#val.pkl')
+parser.add_argument('--test_path', '-test',type=str, default='/rating_2.pkl')#test.pkl')
+
+args = parser.parse_args()
 
 
 
@@ -8,7 +51,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Load the datas
 num_users, num_items, num_classes, num_side_features, num_features,\
-u_features, v_features, u_features_side, v_features_side = get_loader(args.data_type)
+u_features, v_features, u_features_side, v_features_side = data_loader.get_loader(args.data_type)
 
 u_features = torch.from_numpy(u_features).to(device).float()
 v_features = torch.from_numpy(v_features).to(device).float()
